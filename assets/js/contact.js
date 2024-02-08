bccfbaf57393201b05863f0c0d7b6c1e95d6a3a3
oguzhanakca <oguzhan.akca@hotmail.com>
2024-02-08 22:00:57 +0100

    email js implemented,tested,working

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,37 @@
+let sendButton = document.getElementById("send-button");
+
+function eventListeners() {
+  sendButton.addEventListener("click", sendEmail);
+}
+eventListeners();
+// Email JS init
 (function () {
   emailjs.init("e41jrb0pzCdWwUN-m");
 })();
+
+// Email JS Parameters
+
+// Send Email
+function sendEmail(e) {
+  let contactName = document.getElementById("name").value;
+  let contactEmail = document.getElementById("email").value;
+  let contactMessage = document.getElementById("message").value;
+  var templateParams = {
+    from_name: contactName,
+    from_email: contactEmail,
+    message: contactMessage,
+  };
+  emailjs
+    .send("guess_the_word_feedback", "guess_the_word_template", templateParams)
+    .then(
+      (response) => {
+        console.log("SUCCESS!", response.status, response.text);
+      },
+      (error) => {
+        console.log("FAILED...", error);
+      }
+    );
+  e.preventDefault();
+}
+
+function showThankYouScreen() {}
